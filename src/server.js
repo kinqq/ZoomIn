e81608b555f60aa4923a39b8883b143be244d953
kinqq <minkyu070602@gmail.com>
2021-08-12 23:48:28 +0900

    Defalut File Setting

--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
-console.log("Server started! 😍");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(3000);
+const handleListen = () => {
+    console.log(`Listening on http://localhost:3000`);
+};
+app.listen(3000, handleListen);
